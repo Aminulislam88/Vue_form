78c2f9b60b4a655bcc8a87627b7458a063bbda48
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-08-01 11:49:19 +0600

    two way data binding

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <script setup>
-import {ref} from 'vue'
-const color=ref('none')
-function getStyle(){
-    return {
-        backgroundColor:color.value
-    }
-}
+import {ref,reactive} from 'vue'
+const person=reactive({
+    name:"Aminul",
+    age:29,
+    job:"web Developer"
+})
 </script>
 
 <template >
 <section class="mx-auto container">
     <h1 class="text-2xl mb-10"> Vue Form</h1>
-    <p class="pb-5">Your Favorite Color: {{ color }}</p>
-    <p>
-        Your Favorite Color:
-        <input class="p-5" v-model="color" />
-        <div class="w-32 h-32 mt-10" :style="getStyle()"></div>
-    </p>
+    <p class="pb-5">{{ person }}</p>
+    
+    <!-- <div class="flex flex-col mb-5 text-left">
+        <label for="name">Name</label>
+        <input type="text" class="border border-gray-300 runded-md p-2" v-model="person.name"/>
+
+    </div> -->
+    <div class="flex flex-col mb-5 text-left" v-for="(value,key,index) in person" :key="index">
+        <label for="name">{{key}}</label>
+        <input type="text" class="border border-gray-300 runded-md p-2" v-model="person[key]"/>
+
+    </div>
 </section>
 
 
